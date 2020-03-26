2b866fd0171a40e16329bd29916615996cdc90c2
BETR Global <62552662+BETR-Global@users.noreply.github.com>
2020-03-26 08:35:09 +0100

    BETR-Global-4.0 Source codes

---
+++ globalz.py
@@ -0,0 +1,9 @@
+################################################################################
+'''Module "globalz", July 2010, is part of
+BETR-Research by Harald von Waldow <hvwaldow@chem.ethz.ch>, which is
+based on BETR-Global by Matt MacLeod <matthew.macleod@chem.ethz.ch>
+
+This module contains global constants'''
+################################################################################
+
+R=8.314472 # J/(K*mol) gas-constant
